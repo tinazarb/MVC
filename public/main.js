054edb9e3c95eac63cd1ed7763b190b78d7dff60
tinazarb <tinazarb@gmail.com>
2021-06-08 20:49:18 -0400

    updated main.js

--- public/main.js
+++ public/main.js
@@ -4,5 +4,15 @@ update.addEventListener('click', (_) => {
   fetch('/quotes', {
     method: 'put',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify
+    body: JSON.stringify({
+      name: 'Darth Vadar',
+      quote: 'I find your lack of faith disturbing.',
+    }),
   })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      window.location.reload(true);
+    });
+});
