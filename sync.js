61d3b025a94da480095cfc56dbd1d31b1ce2e001
tinazarb <tinazarb@gmail.com>
2021-06-23 22:34:30 -0400

    updated

--- sync.js
+++ sync.js
@@ -1,16 +1,11 @@
 const Rsync = require('rsync')
 
-// Updates environment variables
 require('./dotenv')
 
-// Sync secrets
+
 sync('./secrets', '/var/www/crud-demo')
 
-// ========================
-// Functions
-// ========================
-/**
- * Syncs files into server
+
  * @param {String} source
  * @param {String} destination
  * @param {Boolean} dryRun
@@ -20,7 +15,6 @@ function sync (source, destination, dryRun = false) {
     .shell('ssh')
     .set('stats')
     .flags('avz')
-  // .flags('n') // For dry run.
     .source(source)
     .destination(`${process.env.SSH_USER}@${process.env.SSH_HOST}:${destination}`)
 
@@ -28,16 +22,13 @@ function sync (source, destination, dryRun = false) {
 
   rsync.output(
     function (data) {
-      // do things like parse progress
       const string = Buffer.from(data).toString()
       console.log(string)
     }, function (data) {
-      // do things like parse error output
       console.log(data)
     }
   )
 
-  // Execute the command
   rsync.execute(function (error, code, cmd) {
     if (error) console.error(error)
     console.log(cmd)
